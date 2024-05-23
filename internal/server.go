cd04aaba214c4e6121e8e179d0f77d135ffd49d2
Michal Bradáč <michal.bradac@icloud.com>
2024-05-23 18:17:21 +0200

    implement parameter parsing

--- internal/server.go
+++ internal/server.go
@@ -1,10 +1,14 @@
 package internal
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/prathoss/cards/pkg"
 )
 
@@ -37,17 +41,94 @@ func (s *Server) Run() {
 }
 
 func parseID(r *http.Request) (uuid.UUID, []pkg.InvalidParam) {
-	return uuid.Nil, nil
+	idParamName := "id"
+	var invalidParams []pkg.InvalidParam
+	idStr := r.PathValue(idParamName)
+	if idStr == "" {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   idParamName,
+			Reason: "param not provided",
+		})
+		return uuid.Nil, invalidParams
+	}
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   idParamName,
+			Reason: err.Error(),
+		})
+	}
+	return id, invalidParams
 }
 
 func parseShuffled(r *http.Request) (bool, []pkg.InvalidParam) {
-	return nil, nil
+	shuffledParamName := "shuffled"
+	var invalidParams []pkg.InvalidParam
+	shuffled := false
+
+	if !r.URL.Query().Has(shuffledParamName) {
+		return shuffled, invalidParams
+	}
+
+	shuffledStr := r.URL.Query().Get(shuffledParamName)
+	var err error
+	shuffled, err = strconv.ParseBool(shuffledStr)
+	if err != nil {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   shuffledParamName,
+			Reason: err.Error(),
+		})
+	}
+	return shuffled, invalidParams
 }
 
 func parseCards(r *http.Request) ([]string, []pkg.InvalidParam) {
-	return nil, nil
+	cardsParamName := "cards"
+	var cards []string
+	var invalidParams []pkg.InvalidParam
+
+	cardsStr := r.URL.Query().Get("cards")
+	if cardsStr == "" {
+		return cards, invalidParams
+	}
+	cards = strings.Split(cardsStr, ",")
+	cardsByCode := generateAllCardsCombinationsByCode()
+	for _, card := range cards {
+		if _, ok := cardsByCode[card]; !ok {
+			invalidParams = append(invalidParams, pkg.InvalidParam{
+				Name:   cardsParamName,
+				Reason: fmt.Sprintf("unrecognised card: %s", card),
+			})
+		}
+	}
+	return cards, invalidParams
 }
 
 func parseCount(r *http.Request) (int, []pkg.InvalidParam) {
-	return 0, nil
+	countParamName := "count"
+	var invalidParams []pkg.InvalidParam
+
+	countStr := r.URL.Query().Get(countParamName)
+	if countStr == "" {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   countParamName,
+			Reason: "parameter missing",
+		})
+		return 0, invalidParams
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   countParamName,
+			Reason: err.Error(),
+		})
+	}
+	if count < 1 {
+		invalidParams = append(invalidParams, pkg.InvalidParam{
+			Name:   countParamName,
+			Reason: "count should be greater or equal to 1",
+		})
+	}
+	return count, invalidParams
 }
