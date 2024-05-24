37b23352366123e061d8eb8fc1aef1e28a75e471
Michal Bradáč <michal.bradac@icloud.com>
2024-05-24 10:16:13 +0200

    add deck and cards operations with tests

---
+++ internal/deck.go
@@ -0,0 +1,173 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+const (
+	CardSuitClubs    = "CLUBS"
+	CardSuitDiamonds = "DIAMONDS"
+	CardSuitHearths  = "HEARTS"
+	CardSuitSpades   = "SPADES"
+
+	CardValueAce   = "ACE"
+	CardValueTwo   = "2"
+	CardValueThree = "3"
+	CardValueFour  = "4"
+	CardValueFive  = "5"
+	CardValueSix   = "6"
+	CardValueSeven = "7"
+	CardValueEight = "8"
+	CardValueNine  = "9"
+	CardValueTen   = "10"
+	CardValueJack  = "JACK"
+	CardValueQueen = "QUEEN"
+	CardValueKing  = "KING"
+)
+
+type Card struct {
+	Value string `json:"value"`
+	Suit  string `json:"suit"`
+}
+
+func (c Card) Code() string {
+	v := ""
+	s := ""
+	if _, err := strconv.Atoi(c.Value); err == nil {
+		v = c.Value
+	} else if len(c.Value) > 0 {
+		v = string(c.Value[0])
+	}
+	if len(c.Suit) > 0 {
+		s = string(c.Suit[0])
+	}
+	return fmt.Sprintf("%s%s", v, s)
+}
+
+type Deck struct {
+	ID       uuid.UUID `json:"deck_id"`
+	Shuffled bool      `json:"shuffled"`
+	Cards    []Card    `json:"cards"`
+}
+
+func NewDeck(cardCodes []string, shuffled bool) Deck {
+	cards := generateCards(cardCodes)
+	deck := Deck{
+		ID:       uuid.New(),
+		Shuffled: shuffled,
+		Cards:    cards,
+	}
+	if shuffled {
+		deck.ShuffleCards()
+	}
+	return deck
+}
+
+// ShuffleCards shuffles cards in deck (in place) using Fisher-Yates' algorithm
+func (d *Deck) ShuffleCards() {
+	for i := len(d.Cards) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		d.Cards[i], d.Cards[j] = d.Cards[j], d.Cards[i]
+	}
+}
+
+// generateCards generates a slice of cards based on the provided codes.
+// If the codes slice is empty, it generates all possible combinations of cards.
+// It uses generateAllCardsCombinationsByCode to get the mapping of codes to cards.
+// Unknown card codes will be ignored
+func generateCards(codes []string) []Card {
+	if len(codes) == 0 {
+		return generateAllCardsCombinations()
+	}
+	cardsByCode := generateAllCardsCombinationsByCode()
+	cards := make([]Card, 0, len(codes))
+	for _, code := range codes {
+		if card, ok := cardsByCode[code]; ok {
+			cards = append(cards, card)
+		}
+	}
+	return cards
+}
+
+// generateAllCardsCombinationsByCode generates a mapping of card codes to cards.
+// It uses generateAllCardsCombinations to get all possible combinations of cards.
+// The resulting map maps each code to its corresponding card.
+func generateAllCardsCombinationsByCode() map[string]Card {
+	cards := generateAllCardsCombinations()
+	cardsByCode := make(map[string]Card)
+	for _, c := range cards {
+		cardsByCode[c.Code()] = c
+	}
+	return cardsByCode
+}
+
+// generateAllCardsCombinations generates all possible combinations of cards.
+// It creates cards for each possible suit and value combination and returns them as a slice.
+func generateAllCardsCombinations() []Card {
+	suits := []string{CardSuitClubs, CardSuitDiamonds, CardSuitHearths, CardSuitSpades}
+	values := []string{CardValueAce, CardValueTwo, CardValueThree, CardValueFour, CardValueFive, CardValueSix, CardValueSeven, CardValueEight, CardValueNine, CardValueTen, CardValueJack, CardValueQueen, CardValueKing}
+	cards := make([]Card, 0, len(suits)*len(values))
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, Card{Value: value, Suit: suit})
+		}
+	}
+	return cards
+}
+
+type CreateDeckResponse struct {
+	ID        uuid.UUID `json:"deck_id"`
+	Shuffled  bool      `json:"shuffled"`
+	Remaining int       `json:"remaining"`
+}
+
+func NewCreateDeckResponse(deck Deck) CreateDeckResponse {
+	return CreateDeckResponse{
+		ID:        deck.ID,
+		Shuffled:  deck.Shuffled,
+		Remaining: len(deck.Cards),
+	}
+}
+
+type OpenDeckResponse struct {
+	CreateDeckResponse
+	Cards []CardResponse `json:"cards"`
+}
+
+func NewOpenDeckResponse(deck Deck) OpenDeckResponse {
+	cards := make([]CardResponse, 0, len(deck.Cards))
+	for _, card := range deck.Cards {
+		cards = append(cards, NewCardResponse(card))
+	}
+	return OpenDeckResponse{
+		CreateDeckResponse: NewCreateDeckResponse(deck),
+		Cards:              cards,
+	}
+}
+
+type CardsResponse struct {
+	Cards []CardResponse `json:"cards"`
+}
+
+type CardResponse struct {
+	Card
+	Code string `json:"code"`
+}
+
+func NewCardResponse(c Card) CardResponse {
+	return CardResponse{
+		Card: c,
+		Code: c.Code(),
+	}
+}
+
+type DeckProcessor interface {
+	Create(ctx context.Context, cardsCodes []string, shuffled bool) (Deck, error)
+	Get(ctx context.Context, deckID uuid.UUID) (Deck, error)
+	DrawCards(ctx context.Context, deckID uuid.UUID, count int) ([]Card, error)
+}
---
+++ internal/deck_test.go
@@ -0,0 +1,127 @@
+package internal
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestCard_Code(t *testing.T) {
+	tests := []struct {
+		name string
+		card Card
+		want string
+	}{
+		{
+			name: "NumericValueAndSingleCharSuit",
+			card: Card{
+				Value: CardValueTen,
+				Suit:  CardSuitSpades,
+			},
+			want: "10S",
+		},
+		{
+			name: "AlphabeticSingleCharValueAndSuit",
+			card: Card{
+				Value: CardValueAce,
+				Suit:  CardSuitHearths,
+			},
+			want: "AH",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.card.Code(); got != tt.want {
+				t.Errorf("Card.Code() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func cardsToCodes(cards []Card) []string {
+	resultCodes := make([]string, 0, len(cards))
+	for _, c := range cards {
+		resultCodes = append(resultCodes, c.Code())
+	}
+	return resultCodes
+}
+
+func TestGenerateCards(t *testing.T) {
+	tests := []struct {
+		name  string
+		codes []string
+		want  []string
+	}{
+		{
+			name:  "generate with multiple codes",
+			codes: []string{"AS", "2H", "3D", "4C"},
+			want:  []string{"AS", "2H", "3D", "4C"},
+		},
+		{
+			name:  "generate with single code",
+			codes: []string{"AS"},
+			want:  []string{"AS"},
+		},
+		{
+			name:  "generate with empty code slice",
+			codes: []string{},
+			want:  cardsToCodes(generateAllCardsCombinations()),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := generateCards(tt.codes)
+			resultCodes := cardsToCodes(result)
+			if !slices.Equal(resultCodes, tt.want) {
+				t.Fatalf("got %v, want %v", resultCodes, tt.want)
+			}
+
+		})
+	}
+}
+
+func TestShuffleCards(t *testing.T) {
+	tests := []struct {
+		name  string
+		cards []Card
+	}{
+		{
+			name:  "EmptyDeck",
+			cards: []Card{},
+		},
+		{
+			name: "SingleCardDeck",
+			cards: []Card{
+				{Suit: CardSuitHearths, Value: CardValueAce},
+			},
+		},
+		{
+			name: "MultipleCardDeck",
+			cards: []Card{
+				{Suit: CardSuitClubs, Value: CardValueAce},
+				{Suit: CardSuitHearths, Value: CardValueTen},
+				{Suit: CardSuitSpades, Value: CardValueNine},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			deck := &Deck{Cards: tt.cards}
+			copyBeforeShuffle := make([]Card, len(tt.cards))
+			copy(copyBeforeShuffle, tt.cards)
+			deck.ShuffleCards()
+
+			if len(tt.cards) > 2 {
+				if slices.Equal(deck.Cards, copyBeforeShuffle) {
+					t.Errorf("Deck of cards was not shuffled. Initial and final states are the same.")
+				}
+			} else {
+				if !slices.Equal(deck.Cards, copyBeforeShuffle) {
+					t.Errorf("Deck of cards with zero or one card should stay the same after shuffle.")
+				}
+			}
+		})
+	}
+}
